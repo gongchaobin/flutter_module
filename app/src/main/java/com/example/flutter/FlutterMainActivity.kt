e2f517d0fcce99820d3f1fe846d76ded5c5cf7dd
龚超斌-13120678 <13120678@suning.com>
2020-08-14 11:34:49 +0800

    测试flutter和原生通信(1)

--- app/src/main/java/com/example/flutter/FlutterMainActivity.kt
+++ app/src/main/java/com/example/flutter/FlutterMainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.flutter
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import io.flutter.embedding.android.FlutterFragment
+import io.flutter.plugin.common.BasicMessageChannel
+import io.flutter.plugin.common.StringCodec
+import kotlinx.android.synthetic.main.activity_test.*
 
 /**
  * Copyright (C)
@@ -12,6 +16,14 @@ import io.flutter.embedding.android.FlutterFragment
  * @desc :
  */
 class FlutterMainActivity : AppCompatActivity() {
+
+    companion object {
+
+        private val TAG = FlutterMainActivity::class.simpleName
+
+        private const val ChANNAL = "com.simple.channelled/basic"
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +35,15 @@ class FlutterMainActivity : AppCompatActivity() {
             .add(R.id.container,flutterFragment)
             .commit()
 
+        button.setOnClickListener {
+            // BasicMessageChannel用来传递数据
+            val messenger = flutterFragment.flutterEngine!!.dartExecutor!!.binaryMessenger
+            val basicMessChannel = BasicMessageChannel<String>(messenger, ChANNAL, StringCodec
+                .INSTANCE)
+            basicMessChannel.send("我是Native发送的消息") {
+                Log.i(TAG,"it: $it")
+            }
+        }
     }
 
 }
